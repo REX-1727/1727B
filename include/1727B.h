fd8aad1e574bb237d7a743337638a7a7afe8c71d
apozharski <apozharski@gmail.com>
2016-01-29 11:11:07 -0500

    First commit

---
+++ include/1727B.h
@@ -0,0 +1,23 @@
+/*
+ * 1727B.h
+ *
+ *  Created on: Jan 29, 2016
+ *      Author: Anton
+ */
+
+#ifndef _1727B_H_
+#define _1727B_H_
+
+#include <API.h>
+#include "rexAPI.h"
+#include "math.h"
+
+
+
+bool twoJoysticks;
+
+pidParams leftFlywheel;
+
+pidParams rightFlywheel;
+
+#endif /* _1727B_H_ */
---
+++ src/1727B.c
@@ -0,0 +1,15 @@
+/*
+ * 1727B.c
+ *
+ *  Created on: Jan 29, 2016
+ *      Author: Anton
+ */
+#include "1727B.h"
+
+void powerListener(void *params)
+{
+	while(true)
+	{
+		if(main.)
+	}
+}
